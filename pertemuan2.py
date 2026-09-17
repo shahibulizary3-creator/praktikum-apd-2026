b6c69cdd92d919336923d27091548e10e9904fb2
shahibul izary <shahibul.izary.230307@gmail.com>
2026-09-17 16:29:04 +0800

    jenisntipe data

---
+++ pertemuan2.py
@@ -0,0 +1,38 @@
+# a = 5
+# b = 10
+# c = a + b
+# print(c)
+# nama = "larry"
+# hobi = "ngoding"
+# print (nama)
+# print (hobi)
+
+# mata_kuliah = {"APD", "logika matematika", "kalkulus"}
+# print (mata_kuliah[1])
+
+# data_mahasiswa = ("rafi", "0,34", "true", "3,5")
+# print(data_mahasiswa[0])
+
+# buku = {
+# 'judul' : 'Atomic Habits',
+# 'penulis' : 'James Clear',
+# 'halaman' : 320
+# }
+# ...
+# # Mengakses Value dalam Dictionary
+# print(buku['judul'])
+# print(buku['penulis'])
+# print(buku['halaman'])
+
+# fina = 30
+# difty = "20"
+# tambah = fina + int(difty)
+# print(tambah)
+
+# nama = input("Masukan nama anda: ")
+# print(nama)
+
+a = input("masukkan nilai a")
+b = input("masukkan nilai b")
+c = a * b
+print (c)
